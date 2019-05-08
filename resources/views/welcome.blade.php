fed2d6a891adf84bd74b372065f92bf72e40c390
Dickens-odera <dickensodera9@gmail.com>
2019-05-08 16:44:37 +0300

    Fixixng thr typo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <div class="col-md-6">
         <div class="jumbotron text-center">
             <h4 class="text-center">Simple Registree System</h4>
-            <p><small>This is a simple project to illustrate the Larave CRUD </small></p>
+            <p><small>This is a simple project to illustrate the Laravel CRUD </small></p>
         @if(!Auth::user())
         <a href="{{ route('register') }}" class="btn btn-primary">Get Started</a>
         @else
